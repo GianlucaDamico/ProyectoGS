69aec8ecf0ea5f6fdc95aa48212bb2c0db98e766
Gianluca <gianlucadamico2002@gmail.com>
2026-02-22 13:42:05 +0100

    Templates funcionando (tiene el css incrustado)

--- core/views.py
+++ core/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Página de inicio de Core")
+    return render(request, 'core/home.html')
